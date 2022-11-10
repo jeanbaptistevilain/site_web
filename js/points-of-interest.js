da8aa47b26c971a482e5b6eb1208566f96d40685
Jean-Baptiste Vilain <jbvilain@gmail.com>
2022-11-10 13:52:35 +0100

    added POIs

---
+++ js/points-of-interest.js
@@ -0,0 +1,7 @@
+const pointsOfInterest = [
+  {name: 'Refuge de la Servaz', lat: 45.720019147689136, lng: 6.253936236194966},
+  {name: 'Roc des Boeufs', lat: 45.755970983017846, lng: 6.166909761456816},
+  {name: 'Point de la Gallopaz', lat: 45.567388639634096, lng: 6.066727306183258},
+  {name: 'Croix du Nivolet', lat: 45.61473633696678, lng: 5.965739592698296},
+  {name: 'Croix des Bergers', lat: 45.68691854206205, lng: 6.020031627600368}
+]
