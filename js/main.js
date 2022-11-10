22df049b408f6abd2322c67f7d77fea75171db2e
Jean-Baptiste Vilain <jbvilain@gmail.com>
2022-11-10 14:18:25 +0100

    added examples

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@
 // }).then((data) => {
 //   console.log('data: ' + JSON.stringify(data));
 // });
+
+
+// Pour accéder à la propriété "temp" d'un objet "data" en JS :
+//
+// let maTemperature = data.temp
+//
+// ou
+//
+// let maTemperature = data['temp']
+
+
+// Pour modifier le HTML à partir du code JS :
+//
+// let encart = document.querySelector("#encart_temperature");
+// encart.innerHTML = maTemperature;
